49a9780f017bdd2391fd55f4ca40427440faf9d9
shc <sh@c>
2023-09-08 19:53:47 +0200

    Translate statusline-toggl'g to lua

--- .config/nvim/conf/rpre/plugin/cmd/statusline.lua
+++ .config/nvim/conf/rpre/plugin/cmd/statusline.lua
@@ -1,3 +1,53 @@
+local function hide()
+    vim.opt.laststatus = 0
+    vim.cmd("redraw")
+end
+
+local function show_if_modified()
+    if vim.api.nvim_buf_get_option(0, "modified") then
+        vim.opt.laststatus = 2
+    else
+        vim.opt.laststatus = 0
+    end
+end
+
+local function show(redraw)
+    local function f()
+        vim.opt.laststatus = 2
+        if redraw then
+            vim.cmd("redrawstatus")
+        end
+    end
+
+    return f
+end
+
+local function make_autocmds()
+    local gid = vim.api.nvim_create_augroup(
+        "StatuslineToggle",
+        {clear = true}
+    )
+
+    vim.api.nvim_create_autocmd(
+        {"FileWritePost", "BufWritePost", "CmdlineLeave"},
+        {pattern = {"*"}, group = gid, callback = hide}
+    )
+
+    vim.api.nvim_create_autocmd(
+        {"InsertLeave", "CmdLineLeave", "TextChanged", "TextChangedI", "TextChangedP"},
+        {pattern = {"*"}, group = gid, callback = show_if_modified}
+    )
+
+    vim.api.nvim_create_autocmd(
+        {"InsertEnter"},
+        {pattern = {"*"}, group = gid, callback = show(false)}
+    )
+    vim.api.nvim_create_autocmd(
+        {"CmdlineEnter"},
+        {pattern = {"*"}, group = gid, callback = show(true)}
+    )
+end
+
 local function statusline()
     vim.opt.ruler = false  -- use our own cursor-coordinate display instead
 
@@ -8,6 +58,7 @@ local function statusline()
 end
 
 local function main()
+    make_autocmds()
     statusline()
 end
 main()
