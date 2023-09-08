8bf2ba447b0a4584974aeb8db38ddcac1a072784
shc <sh@c>
2023-09-08 18:12:12 +0200

    Rework to edit.lua

---
+++ .config/nvim/conf/rpre/plugin/edit.lua
@@ -0,0 +1,42 @@
+local function format()
+    vim.opt.textwidth = 79
+
+    vim.opt.autoindent = true
+
+    vim.opt.expandtab = true  -- spaces instead of tabs
+    vim.opt.smarttab = true
+    vim.opt.shiftround = true
+
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 4
+    vim.opt.shiftwidth = 4
+
+    local function remove_trailing()
+        pos = vim.api.nvim_win_get_cursor(0)
+
+        --  remove leading with %s/^\s\+//e
+        vim.cmd([[%s/\s\+$//e]])
+
+        vim.api.nvim_win_set_cursor(0, pos)
+    end
+    vim.api.nvim_create_autocmd({"BufWritePre"}, {
+        pattern = {"*"},
+        callback = remove_trailing,
+    })
+end
+
+local function cmd()
+    vim.opt.undolevels = 1000
+
+    vim.opt.clipboard:append("unnamedplus")  -- use system clipboard
+
+    -- allow buffering even if unwritten modifications exist
+    vim.opt.hidden = true
+end
+
+local function main()
+    format()
+    cmd()
+end
+main()
+
