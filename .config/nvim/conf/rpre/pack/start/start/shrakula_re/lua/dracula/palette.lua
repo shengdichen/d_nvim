97957712c23c3a43985590e2ca8da1f6d96acd1b
shc <sh@c>
2023-09-09 22:40:24 +0200

    Initial rewrite palette.lua

--- .config/nvim/conf/rpre/pack/start/start/shrakula_re/lua/dracula/palette.lua
+++ .config/nvim/conf/rpre/pack/start/start/shrakula_re/lua/dracula/palette.lua
@@ -1,27 +1,42 @@
----@class Palette
-return {
-    bg = "None",
-    fg = "#ede3f7",
-    selection = "#44475A",
-    comment = "#6272A4",
-    red = "#FF5555",
-    orange = "#FFB86C",
-    yellow = "#F1FA8C",
-    green = "#50fa7b",
-    purple = "#BD93F9",
-    cyan = "#8BE9FD",
-    pink = "#FF79C6",
-    bright_red = "#FF6E6E",
-    bright_green = "#69FF94",
-    bright_yellow = "#FFFFA5",
-    bright_blue = "#D6ACFF",
-    bright_magenta = "#FF92DF",
-    bright_cyan = "#A4FFFF",
-    bright_white = "#FFFFFF",
-    menu = "#21222C",
-    visual = "#3E4452",
-    gutter_fg = "#4B5263",
-    nontext = "#3B4048",
-    white = "#ABB2BF",
-    black = "#191A21",
-}
+local function make_greyscale(palette)
+    palette["bg"] = "None"
+    palette["fg"] = "None"
+
+    palette["selection"] = "#44475A"
+    palette["comment"] = "#6272A4"
+
+    palette["menu"] = "#21222C"
+    palette["visual"] = "#3E4452"
+    palette["gutter_fg"] = "#4B5263"
+    palette["nontext"] = "#3B4048"
+    palette["white"] = "#ABB2BF"
+    palette["black"] = "#191A21"
+end
+
+local function make_color(palette)
+    palette["red"] = "#FF5555"
+    palette["orange"] = "#FFB86C"
+    palette["yellow"] = "#F1FA8C"
+    palette["green"] = "#50fa7b"
+    palette["purple"] = "#BD93F9"
+    palette["cyan"] = "#8BE9FD"
+    palette["pink"] = "#FF79C6"
+
+    palette["bright_red"] = "#FF6E6E"
+    palette["bright_green"] = "#69FF94"
+    palette["bright_yellow"] = "#FFFFA5"
+    palette["bright_blue"] = "#D6ACFF"
+    palette["bright_magenta"] = "#FF92DF"
+    palette["bright_cyan"] = "#A4FFFF"
+    palette["bright_white"] = "#FFFFFF"
+end
+
+local function main()
+    local palette = {}
+
+    make_greyscale(palette)
+    make_color(palette)
+
+    return palette
+end
+return main()
