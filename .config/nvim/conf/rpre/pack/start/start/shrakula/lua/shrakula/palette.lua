4a6e3e8e909142bcf167b7670947bc046eb73e12
shc <sh@c>
2023-09-10 20:55:12 +0200

    Separate normal&bright color creat'n

--- .config/nvim/conf/rpre/pack/start/start/shrakula/lua/shrakula/palette.lua
+++ .config/nvim/conf/rpre/pack/start/start/shrakula/lua/shrakula/palette.lua
@@ -5,7 +5,7 @@ local function make_greyscale(palette)
     palette["white_bright"] = "#ede3f7"
 end
 
-local function make_color(palette)
+local function make_color_normal(palette)
     palette["red"] = "#ff5555"
     palette["green"] = "#50fa7b"
     palette["yellow"] = "#f1fa8c"
@@ -13,21 +13,24 @@ local function make_color(palette)
     palette["magenta"] = "#ff79c6"
     palette["cyan"] = "#8be9fd"
 
+    palette["orange"] = "#ffb86c" -- non-16 color
+end
+
+local function make_color_bright(palette)
     palette["red_bright"] = "#ff6e6e"
     palette["green_bright"] = "#69ff94"
     palette["yellow_bright"] = "#ffffa5"
     palette["purple_bright"] = "#d6acff"
     palette["magenta_bright"] = "#ff92df"
     palette["cyan_bright"] = "#a4ffff"
-
-    palette["orange"] = "#ffb86c"
 end
 
 local function main()
     local palette = {}
 
     make_greyscale(palette)
-    make_color(palette)
+    make_color_normal(palette)
+    make_color_bright(palette)
 
     return palette
 end
