95e7c50152bb76fa0569a4354d5df02de0e933b2
shc <sh@c>
2023-09-10 19:48:56 +0200

    Use index-syntax for set'g term-color

--- .config/nvim/conf/rpre/pack/start/start/shrakula/lua/shrakula/init.lua
+++ .config/nvim/conf/rpre/pack/start/start/shrakula/lua/shrakula/init.lua
@@ -31,25 +31,25 @@ local function set_color_true(palette)
 end
 
 local function set_color_256(palette)
-    vim.g.terminal_color_0 = palette.black
-    vim.g.terminal_color_1 = palette.red
-    vim.g.terminal_color_2 = palette.green
-    vim.g.terminal_color_3 = palette.yellow
-    vim.g.terminal_color_4 = palette.purple
-    vim.g.terminal_color_5 = palette.pink
-    vim.g.terminal_color_6 = palette.cyan
-    vim.g.terminal_color_7 = palette.white
-    vim.g.terminal_color_8 = palette.selection
-    vim.g.terminal_color_9 = palette.bright_red
-    vim.g.terminal_color_10 = palette.bright_green
-    vim.g.terminal_color_11 = palette.bright_yellow
-    vim.g.terminal_color_12 = palette.bright_blue
-    vim.g.terminal_color_13 = palette.bright_magenta
-    vim.g.terminal_color_14 = palette.bright_cyan
-    vim.g.terminal_color_15 = palette.bright_white
+    vim.g.terminal_color_0 = palette["black"]
+    vim.g.terminal_color_1 = palette["red"]
+    vim.g.terminal_color_2 = palette["green"]
+    vim.g.terminal_color_3 = palette["yellow"]
+    vim.g.terminal_color_4 = palette["purple"]
+    vim.g.terminal_color_5 = palette["pink"]
+    vim.g.terminal_color_6 = palette["cyan"]
+    vim.g.terminal_color_7 = palette["white"]
+    vim.g.terminal_color_8 = palette["selection"]
+    vim.g.terminal_color_9 = palette["bright_red"]
+    vim.g.terminal_color_10 = palette["bright_green"]
+    vim.g.terminal_color_11 = palette["bright_yellow"]
+    vim.g.terminal_color_12 = palette["bright_blue"]
+    vim.g.terminal_color_13 = palette["bright_magenta"]
+    vim.g.terminal_color_14 = palette["bright_cyan"]
+    vim.g.terminal_color_15 = palette["bright_white"]
 
-    vim.g.terminal_color_background = palette.bg
-    vim.g.terminal_color_foreground = palette.fg
+    vim.g.terminal_color_background = palette["bg"]
+    vim.g.terminal_color_foreground = palette["fg"]
 end
 
 local function main()
