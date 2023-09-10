5b76a6bde3e07b0e288359fbfbd823d7baf8b90b
shc <sh@c>
2023-09-10 20:13:50 +0200

    Separate set'g for 16-color

--- .config/nvim/conf/rpre/pack/start/start/shrakula/lua/shrakula/init.lua
+++ .config/nvim/conf/rpre/pack/start/start/shrakula/lua/shrakula/init.lua
@@ -30,36 +30,13 @@ local function set_color_true(palette)
     end
 end
 
-local function set_color_256(palette)
-    vim.g.terminal_color_background = "none"
-    vim.g.terminal_color_foreground = "none"
-
-    vim.g.terminal_color_0 = palette["black_dark"]
-    vim.g.terminal_color_1 = palette["red"]
-    vim.g.terminal_color_2 = palette["green"]
-    vim.g.terminal_color_3 = palette["yellow"]
-    vim.g.terminal_color_4 = palette["purple"]
-    vim.g.terminal_color_5 = palette["magenta"]
-    vim.g.terminal_color_6 = palette["cyan"]
-    vim.g.terminal_color_7 = palette["white_dark"]
-
-    vim.g.terminal_color_8 = palette["black_bright"]
-    vim.g.terminal_color_9 = palette["red_bright"]
-    vim.g.terminal_color_10 = palette["green_bright"]
-    vim.g.terminal_color_11 = palette["yellow_bright"]
-    vim.g.terminal_color_12 = palette["purple_bright"]
-    vim.g.terminal_color_13 = palette["magenta_bright"]
-    vim.g.terminal_color_14 = palette["cyan_bright"]
-    vim.g.terminal_color_15 = palette["white_bright"]
-end
-
 local function main()
     if not check_version() then return end
     setup_vim()
 
     local palette = require("shrakula.palette")
     set_color_true(palette)
-    set_color_256(palette)
+    require("shrakula.color_16")(palette)
 end
 
 return main
