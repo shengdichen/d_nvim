b7327e8e9c3338e1a3e1e45af5ed3995c09d70cd
shc <sh@c>
2023-09-13 09:23:52 +0200

    Cleanup binds for navigat'n

--- .config/nvim/conf/rpre/plugin/bind/navigate.lua
+++ .config/nvim/conf/rpre/plugin/bind/navigate.lua
@@ -1,33 +1,31 @@
+local modes = { "n", "v" }
+
 local function move_as_seen()
     -- NOTE:
     --  1. particularly useful for prose-form
 
-    vim.keymap.set({ "n", "v" }, "j", "gj")
-    vim.keymap.set({ "n", "v" }, "k", "gk")
+    vim.keymap.set(modes, "j", "gj")
+    vim.keymap.set(modes, "k", "gk")
+    vim.keymap.set(modes, "J", "4gj")
+    vim.keymap.set(modes, "K", "4gk")
 
-    vim.keymap.set({ "n", "v" }, "0", "g0")
-    vim.keymap.set({ "n", "v" }, "$", "g$")
+    vim.keymap.set(modes, "0", "g0")
+    vim.keymap.set(modes, "$", "g$")
+
+    -- true beginning/end of lines, ignoring all leading/trailing spaces
+    vim.keymap.set(modes, "H", "g^")
+    vim.keymap.set(modes, "L", "g_")
 end
 
-local function vertical()
-    vim.keymap.set({ "n", "v" }, "J", "4gj")
-    vim.keymap.set({ "n", "v" }, "K", "4gk")
-
-    vim.keymap.set({ "n", "v" }, "<Up>", "")
-    vim.keymap.set({ "n", "v" }, "<Down>", "")
-    vim.keymap.set({ "n", "v" }, "<PageUp>", "")
-    vim.keymap.set({ "n", "v" }, "<PageUp>", "")
-end
-
-local function horizontal()
-    -- the true beginning (end) of lines, ignoring all leading (trailing) spaces
-    vim.keymap.set({ "n", "v" }, "H", "g^")
-    vim.keymap.set({ "n", "v" }, "L", "g_")
+local function unbind()
+    vim.keymap.set(modes, "<Up>", "")
+    vim.keymap.set(modes, "<Down>", "")
+    vim.keymap.set(modes, "<PageUp>", "")
+    vim.keymap.set(modes, "<PageUp>", "")
 end
 
 local function main()
     move_as_seen()
-    vertical()
-    horizontal()
+    unbind()
 end
 main()
