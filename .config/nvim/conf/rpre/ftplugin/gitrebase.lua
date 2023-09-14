21d4fa67b656eee62a583cccb5bc71a5df455306
shc <sh@c>
2023-09-14 07:28:13 +0200

    Add bind for inspect'g commit in new split

--- .config/nvim/conf/rpre/ftplugin/gitrebase.lua
+++ .config/nvim/conf/rpre/ftplugin/gitrebase.lua
@@ -2,8 +2,25 @@ local function visual()
     vim.opt_local.laststatus = 2
 end
 
+local function get_current_word()
+    -- |:help expand()|
+    return vim.fn.expand("<cword>")
+end
+
+local function inspect_commit()
+    cmd = "git show " .. get_current_word()
+
+    vim.cmd("wincmd l | split")
+    vim.cmd("terminal $SHELL -c " .. '"' .. cmd .. '"')
+
+    vim.cmd("wincmd h")
+    vim.cmd("stopinsert")
+end
+
 local function bind()
     vim.keymap.set("c", "wq", "w <bar> qa", { buffer = 0 })
+
+    vim.keymap.set("n", "<Enter>", inspect_commit, { buffer = 0 })
 end
 
 local function main()
