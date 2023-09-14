b6e4ccd394be9594491908fb662e3e0c597bbeec
shc <sh@c>
2023-09-14 23:52:09 +0200

    Cleanup navigate.lua

--- .config/nvim/conf/rpre/plugin/bind/navigate.lua
+++ .config/nvim/conf/rpre/plugin/bind/navigate.lua
@@ -1,48 +1,47 @@
 -- NOTE:
 --  1. find name for keys from |:h key-notation|
 
-local _unbind = require("bind")["unbind"]
-
-local modes = { "n", "v" }
+local _unbind         = require("bind")["unbind"]
+local mode_nv, mode_i = { "n", "v" }, { "i" }
 
 local function move_as_seen()
     -- NOTE:
     --  1. particularly useful for prose-form
 
-    vim.keymap.set(modes, "j", "gj")
-    vim.keymap.set(modes, "k", "gk")
-    vim.keymap.set(modes, "J", "4gj")
-    vim.keymap.set(modes, "K", "4gk")
+    vim.keymap.set(mode_nv, "j", "gj")
+    vim.keymap.set(mode_nv, "k", "gk")
+    vim.keymap.set(mode_nv, "J", "4gj")
+    vim.keymap.set(mode_nv, "K", "4gk")
 
-    vim.keymap.set(modes, "0", "g0")
-    vim.keymap.set(modes, "$", "g$")
+    vim.keymap.set(mode_nv, "0", "g0")
+    vim.keymap.set(mode_nv, "$", "g$")
 
     -- true beginning/end of lines, ignoring all leading/trailing spaces
-    vim.keymap.set(modes, "H", "g^")
-    vim.keymap.set(modes, "L", "g_")
+    vim.keymap.set(mode_nv, "H", "g^")
+    vim.keymap.set(mode_nv, "L", "g_")
 end
 
 local function unbind()
-    _unbind(modes, "<Up>")
-    _unbind(modes, "<Down>")
-    _unbind(modes, "<PageUp>")
-    _unbind(modes, "<PageUp>")
+    _unbind(mode_nv, "<Up>")
+    _unbind(mode_nv, "<Down>")
+    _unbind(mode_nv, "<PageUp>")
+    _unbind(mode_nv, "<PageUp>")
 end
 
 local function insert()
-    _unbind("i", "<PageUp>")
-    _unbind("i", "<PageDown>")
+    _unbind(mode_i, "<PageUp>")
+    _unbind(mode_i, "<PageDown>")
 
-    vim.keymap.set("i", "<C-h>", "<Left>")
-    vim.keymap.set("i", "<C-j>", "<Down>")
-    vim.keymap.set("i", "<C-k>", "<Up>")
-    vim.keymap.set("i", "<C-l>", "<Right>")
+    vim.keymap.set(mode_i, "<C-h>", "<Left>")
+    vim.keymap.set(mode_i, "<C-j>", "<Down>")
+    vim.keymap.set(mode_i, "<C-k>", "<Up>")
+    vim.keymap.set(mode_i, "<C-l>", "<Right>")
 
-    vim.keymap.set("i", "<C-b>", "<C-Left>")
-    vim.keymap.set("i", "<C-w>", "<C-Right>")
+    vim.keymap.set(mode_i, "<C-b>", "<C-Left>")
+    vim.keymap.set(mode_i, "<C-w>", "<C-Right>")
 
-    vim.keymap.set("i", "<C-e>", "<ScrollWheelDown>")
-    vim.keymap.set("i", "<C-y>", "<ScrollWheelUp>")
+    vim.keymap.set(mode_i, "<C-e>", "<ScrollWheelDown>")
+    vim.keymap.set(mode_i, "<C-y>", "<ScrollWheelUp>")
 end
 
 local function main()
