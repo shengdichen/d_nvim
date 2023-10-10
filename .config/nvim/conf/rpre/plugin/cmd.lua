b5e3806eeba2acbbc49ee4079da1dd8374494b1e
shc <sh@c>
2023-10-10 04:06:21 +0200

    Add init-implement'n for split'g to tmux

--- .config/nvim/conf/rpre/plugin/cmd.lua
+++ .config/nvim/conf/rpre/plugin/cmd.lua
@@ -29,9 +29,29 @@ local function search()
     vim.opt.smartcase = true
 end
 
+local function split_to_tmux()
+    local function f()
+        local n_splits_active = vim.fn.tabpagewinnr(vim.fn.tabpagenr(), "$")
+        if n_splits_active > 1 then
+            local pos = vim.api.nvim_win_get_cursor(0)
+            local cmd_set_cursor = "'" .. "call cursor(" .. pos[1] .. ", " .. pos[2] .. ")" .. "'"
+            local curr_file = "'" .. vim.api.nvim_buf_get_name(0) .. "'"
+            local cmd_nvim = '"' .. "nvim -c " .. cmd_set_cursor .. " -- " .. curr_file .. '"'
+
+            vim.api.nvim_buf_delete(0, {})
+            os.execute(table.concat(
+                { "tmux", "split-window", cmd_nvim }, " "
+            ))
+        end
+    end
+
+    vim.keymap.set("c", ":Q", f)
+end
+
 local function main()
     terminal()
     commandline()
     search()
+    split_to_tmux()
 end
 main()
