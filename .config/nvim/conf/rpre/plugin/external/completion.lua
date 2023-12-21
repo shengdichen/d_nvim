f881e60c66496403d2701a89b2de4d9d8a451e2f
shc <sh@c>
2023-12-21 16:07:07 +0100

    feat(snippet): basic bash

--- .config/nvim/conf/rpre/plugin/external/completion.lua
+++ .config/nvim/conf/rpre/plugin/external/completion.lua
@@ -2,6 +2,239 @@ local c = {}
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
+local function snippets_collection()
+    local spt = luasnip.snippet
+    local n_sn = luasnip.snippet_node
+    local n_is = luasnip.indent_snippet_node
+
+    local n_t = luasnip.text_node
+    local line_break = function(count)
+        local text = {}
+        for _ = 0, count do
+            table.insert(text, "")
+        end
+        return n_t(text)
+    end
+    local tab = function(count)
+        local text = string.rep(" ", count * 4)
+        return n_t(text)
+    end
+    local n_i = luasnip.insert_node
+    local n_f = luasnip.function_node
+
+    local n_c = luasnip.choice_node
+    local n_d = luasnip.dynamic_node
+    local n_r = luasnip.restore_node
+
+    local s_sh = {
+        spt("var",
+            n_c(1, {
+                { n_t({ '"${' }), n_i(1, "var?"), n_t({ '}"' }), },
+                { n_t({ "${" }),  n_i(1, "var?"), n_t({ "}" }), },
+            })
+        ),
+        spt("v#",
+            n_c(1, {
+                { n_t({ '"${#}"' }) },
+                { n_t({ "${#}" }) },
+            })
+        ),
+        spt("v1",
+            n_c(1, {
+                { n_t({ '"${1}"' }) },
+                { n_t({ "${1}" }) },
+            })
+        ),
+        spt("v2",
+            n_c(1, {
+                { n_t({ '"${2}"' }) },
+                { n_t({ "${2}" }) },
+            })
+        ),
+        spt("v3",
+            n_c(1, {
+                { n_t({ '"${3}"' }) },
+                { n_t({ "${3}" }) },
+            })
+        ),
+        spt("v@",
+            n_c(1, {
+                { n_t({ '"${@}"' }) },
+                { n_t({ "${@}" }) },
+            })
+        ),
+        spt("v*",
+            n_c(1, {
+                { n_t({ '"${*}"' }) },
+                { n_t({ "${*}" }) },
+            })
+        ),
+
+        spt("fn",
+            {
+                n_i(1, "fn?"), n_t({ "() {" }),
+                line_break(1),
+                tab(1), n_i(2, "what?"),
+                line_break(1),
+                n_t({ "}" }),
+                line_break(1),
+            }
+        ),
+        spt("sub",
+            {
+                n_t({ "(" }),
+                line_break(1),
+                tab(1), n_i(1, "what?"),
+                line_break(1),
+                n_t({ ")" }),
+                line_break(1),
+            }
+        ),
+        spt("cd",
+            n_c(1, {
+                {
+                    n_t({ "cd " }), n_i(1, "where?"), n_t({ " || exit " }), n_i(2, "3"),
+                    line_break(1),
+                },
+                {
+                    n_t({ "cd " }), n_i(1, "where?"), n_t({ " && " }), n_i(2, "what?"),
+                },
+            })
+        ),
+
+        --  if $1; then
+        --      $0
+        --  fi
+        spt("if",
+            {
+                n_t({ "if " }), n_i(1, "test?"), n_t({ "; then" }),
+                line_break(1),
+                tab(1), n_i(2, "what?"),
+                line_break(1),
+                n_t({ "fi" }),
+                line_break(1),
+            }
+        ),
+        spt("if--",
+            { n_t({ 'if [ "${1}" = "--" ]; then shift; fi' }), }
+        ),
+
+        spt("tt",
+            { n_t({ "[ " }), n_i(1, "test?"), n_t({ " ]" }), }
+        ),
+        -- test: os
+        spt("te",
+            n_c(1, {
+                { n_t({ "[ -e " }),   n_i(1, "path?"), n_t({ " ]" }), },
+                { n_t({ "[ ! -e " }), n_i(1, "path?"), n_t({ " ]" }), },
+            })
+        ),
+        spt("tf",
+            n_c(1, {
+                { n_t({ "[ -f " }),   n_i(1, "path?"), n_t({ " ]" }), },
+                { n_t({ "[ ! -f " }), n_i(1, "path?"), n_t({ " ]" }), },
+            })
+        ),
+        spt("td",
+            n_c(1, {
+                { n_t({ "[ -d " }),   n_i(1, "path?"), n_t({ " ]" }), },
+                { n_t({ "[ ! -d " }), n_i(1, "path?"), n_t({ " ]" }), },
+            })
+        ),
+        --  test: string
+        spt("tstr",
+            n_c(1, {
+                { n_t({ "[ " }),   n_i(1, "str?"), n_t({ " ]" }), },
+                { n_t({ "[ ! " }), n_i(1, "str?"), n_t({ " ]" }), },
+            })
+        ),
+        spt("tstrtrue",
+            { n_t({ "[ -n " }), n_i(1, "str?"), n_t({ " ]" }), }
+        ),
+        spt("tstrfalse",
+            { n_t({ "[ -z " }), n_i(1, "str?"), n_t({ " ]" }), }
+        ),
+        spt("tstr==",
+            { n_t({ "[ " }), n_i(1, "str?"), n_t({ " = " }), n_i(2, "value"), n_t({ " ]" }), }
+        ),
+        spt("tstr!=",
+            { n_t({ "[ " }), n_i(1, "str?"), n_t({ " != " }), n_i(2, "value"), n_t({ " ]" }), }
+        ),
+        -- test: algebraic
+        spt("t==",
+            { n_t({ "[ " }), n_i(1, "var?"), n_t({ " -eq " }), n_i(2, "value"), n_t({ " ]" }), }
+        ),
+        spt("t!=",
+            { n_t({ "[ " }), n_i(1, "var?"), n_t({ " -ne " }), n_i(2, "value"), n_t({ " ]" }), }
+
+        ),
+        spt("t>",
+            { n_t({ "[ " }), n_i(1, "var?"), n_t({ " -gt " }), n_i(2, "value"), n_t({ " ]" }), }
+        ),
+        spt("t>=",
+            { n_t({ "[ " }), n_i(1, "var?"), n_t({ " -ge " }), n_i(2, "value"), n_t({ " ]" }), }
+        ),
+        spt("t<",
+            { n_t({ "[ " }), n_i(1, "var?"), n_t({ " -lt " }), n_i(2, "value"), n_t({ " ]" }), }
+        ),
+        spt("t<=",
+            { n_t({ "[ " }), n_i(1, "var?"), n_t({ " -le " }), n_i(2, "value"), n_t({ " ]" }), }
+        ),
+
+        spt("while",
+            {
+                n_t("while "), n_i(1, "test?"), n_t("; do"),
+                line_break(1),
+                tab(1), n_i(2, "what?"),
+                line_break(1),
+                n_t("done"),
+                line_break(1),
+                n_i(0)
+            }
+        ),
+        spt("case",
+            {
+                n_t("case "), n_i(1, "var?"), n_t(" in"),
+                line_break(1),
+
+                tab(1), n_t('"'), n_i(2, "val1?"), n_t('")'),
+                line_break(1),
+                tab(2), n_i(3, "what?"),
+                line_break(1),
+                tab(2), n_t(";;"),
+                line_break(1),
+
+                tab(1), n_t('"'), n_i(4, "val2?"), n_t('")'),
+                line_break(1),
+                tab(2), n_i(5, "what?"),
+                line_break(1),
+                tab(2), n_t(";;"),
+                line_break(1),
+
+                tab(1), n_t("*)"),
+                line_break(1),
+                tab(2), n_i(6, "exit 3"),
+                line_break(1),
+                tab(2), n_t(";;"),
+                line_break(1),
+
+                n_t("esac"),
+                line_break(1),
+                n_i(0)
+            }
+        ),
+
+        spt("shebang",
+            {
+                n_t({ "#!/usr/bin/env " }), n_i(1, "da"), n_t({ "sh" }),
+                line_break(1),
+            }
+        ),
+
+    }
+    luasnip.add_snippets("sh", s_sh)
+end
+
 local function snippet()
     c["snippet"] = {
         expand = function(args) luasnip.lsp_expand(args.body) end
@@ -63,10 +296,22 @@ local function map()
     })
     c_map["<c-e>"] = cmp.mapping.abort()
 
+    vim.keymap.set(
+        { "i", "s" },
+        "<c-r>",
+        function()
+            if luasnip.choice_active() then
+                luasnip.change_choice(1)
+            end
+        end,
+        { silent = true }
+    )
+
     c["mapping"] = cmp.mapping.preset.insert(c_map)
 end
 
 local function main()
+    snippets_collection()
     snippet()
     source()
     map()
