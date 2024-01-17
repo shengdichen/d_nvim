e62c10dfb087e7a6c5283c3943a58137eeaf502f
shc <sh@c>
2024-01-17 17:52:35 +0100

    chore(fold): relocate to treesitter-setup site

--- .config/nvim/conf/rpre/plugin/external/treesitter.lua
+++ .config/nvim/conf/rpre/plugin/external/treesitter.lua
@@ -11,10 +11,23 @@ local function highlight(conf)
     }
 end
 
+local function fold()
+    -- REF:
+    --  https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#folding
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+    -- NOTE:
+    --  1. just enough for one indent-level in a (class-)method
+    --  2. use |vim.opt.foldenable = false| for full expansion
+    vim.opt.foldlevel = 3
+end
+
 local function main()
     local conf = {}
     install(conf)
     highlight(conf)
+    fold()
 
     require("nvim-treesitter.configs").setup(conf)
 end
