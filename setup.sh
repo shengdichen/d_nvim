c76001940386c9d1a315550c7bb4b9b8fc415c61
shc <sh@c>
2024-01-19 21:06:55 +0100

    feat(setup): --update flag

--- setup.sh
+++ setup.sh
@@ -3,29 +3,57 @@
 SCRIPT_PATH="$(realpath "$(dirname "${0}")")"
 cd "${SCRIPT_PATH}" || exit 3
 
-__clone() {
-    _link="https://github.com/${1}/${2}.git"
+__install() {
+    local _update
+    if [ "${1}" = "--update" ]; then
+        _update="yes"
+    fi
+    shift
 
     if [ ! -d "${2}" ]; then
-        # cater for failed cloning (bad permission, wrong address...)
-        if ! git clone --depth 3 "${_link}"; then
-            echo "Cloning [${_link}] failed, exiting"
-            exit 3
+        __clone "${1}" "${2}"
+    else
+        if [ "${_update}" ]; then
+            __update "${2}"
         fi
     fi
 }
 
-__plugin() {
-    __lsp() {
-        __clone "neovim" "nvim-lspconfig"
-        __clone "nvimtools" "none-ls.nvim"
+__clone() {
+    local _link="https://github.com/${1}/${2}.git"
 
-        __clone "folke" "neodev.nvim"
-        __clone "j-hui" "fidget.nvim"
+    echo "clone> ${_link}"
+    if ! git clone --depth 3 "${_link}"; then
+        echo "clone> [${_link}] failed, bad internet/permission/address? (exiting)"
+        exit 3
+    fi
+}
+
+__update() {
+    (
+        cd "${1}" || exit 3
+        echo "update> ${1}"
+        if ! (git fetch && git merge); then
+            echo "update> failed, bad internet? (exiting)"
+            exit 3
+        fi
+    )
+}
+
+__plugin() {
+    local _update="${1}"
+    shift
+
+    __lsp() {
+        __install "${_update}" "neovim" "nvim-lspconfig"
+        __install "${_update}" "nvimtools" "none-ls.nvim"
+
+        __install "${_update}" "folke" "neodev.nvim"
+        __install "${_update}" "j-hui" "fidget.nvim"
     }
 
     __snippet() {
-        __clone "L3MON4D3" "LuaSnip"
+        __install "${_update}" "L3MON4D3" "LuaSnip"
         (
             # REF:
             #   https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#transformations
@@ -37,30 +65,30 @@ __plugin() {
         )
 
         # snippets collection
-        __clone "rafamadriz" "friendly-snippets"
+        __install "${_update}" "rafamadriz" "friendly-snippets"
     }
 
     __completion() {
-        __clone "hrsh7th" "nvim-cmp"
+        __install "${_update}" "hrsh7th" "nvim-cmp"
 
         # integration with neovim's (builtin) lsp
-        __clone "hrsh7th" "cmp-nvim-lsp"
+        __install "${_update}" "hrsh7th" "cmp-nvim-lsp"
 
         # integration with luasnip
-        __clone "saadparwaiz1" "cmp_luasnip"
+        __install "${_update}" "saadparwaiz1" "cmp_luasnip"
     }
 
     __syntax() {
-        __clone "nvim-treesitter" "nvim-treesitter"
-        __clone "lukas-reineke" "indent-blankline.nvim"
+        __install "${_update}" "nvim-treesitter" "nvim-treesitter"
+        __install "${_update}" "lukas-reineke" "indent-blankline.nvim"
     }
 
     __misc() {
-        __clone "nvim-lua" "plenary.nvim"
-        __clone "lewis6991" "gitsigns.nvim"
+        __install "${_update}" "nvim-lua" "plenary.nvim"
+        __install "${_update}" "lewis6991" "gitsigns.nvim"
 
-        __clone "tpope" "vim-surround"
-        __clone "vifm" "vifm.vim"
+        __install "${_update}" "tpope" "vim-surround"
+        __install "${_update}" "vifm" "vifm.vim"
     }
 
     (
@@ -83,11 +111,19 @@ __stow() {
 }
 
 main() {
-    __plugin
+    local _update
+    case "${1}" in
+        "--update" | "-u")
+            _update="--update"
+            shift
+            ;;
+    esac
+
+    __plugin "${_update}"
     __stow
 
     unset SCRIPT_PATH
     unset -f __clone __plugin __stow
 }
-main
+main "${@}"
 unset -f main
