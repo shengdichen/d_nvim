bbb0315750593af4926202fff759077ac7049974
shc <sh@c>
2024-03-31 15:19:02 +0200

    chore(util): add typehints

--- .config/nvim/conf/rpre/lua/internal.lua
+++ .config/nvim/conf/rpre/lua/internal.lua
@@ -1,13 +1,20 @@
 local MODULE = {}
 
+---@param mode string|table
+---@param lhs string
+---@param args table|nil
 MODULE.unbind = function(mode, lhs, args)
     vim.keymap.set(mode, lhs, "", args)
 end
 
+---@param cmd string
 MODULE.run_in_terminal = function(cmd)
     vim.cmd("terminal $SHELL -c " .. '"' .. cmd .. '"')
 end
 
+---@param enable boolean
+---@param langs string|table|nil
+---@return fun(): nil
 MODULE.spell = function(enable, langs)
     return function()
         if enable then
@@ -26,6 +33,7 @@ MODULE.spell = function(enable, langs)
     end
 end
 
+---@param level integer
 MODULE.statusline = function(level)
     vim.opt_local.laststatus = level
 end
