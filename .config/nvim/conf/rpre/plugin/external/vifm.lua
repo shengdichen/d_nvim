59fc57a658f805d9b3da47ff92b81c29c70f6a58
shc <sh@c>
2024-03-31 14:27:46 +0200

    feat(vifm): add base setup

--- .config/nvim/conf/rpre/plugin/external/vifm.lua
+++ .config/nvim/conf/rpre/plugin/external/vifm.lua
@@ -1,9 +1,15 @@
+local function conf()
+    vim.g.vifm_embed_cwd = true
+    vim.g.vifm_exec_args = "-c 'source $VIFM/conf/misc/nvim.vifm'"
+end
+
 local function bind()
     vim.keymap.set("c", ":V", "Vifm")
     vim.keymap.set("c", ":S", "SplitVifm")
 end
 
 local function main()
+    conf()
     bind()
 end
 main()
