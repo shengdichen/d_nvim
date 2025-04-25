cb70912afe06fd0d82569a36086b2aa312ca2d22
shc <sh@c>
2025-04-25 17:14:04 +0200

    feat(conf): add windows/init.lua

---
+++ windows/init.lua
@@ -0,0 +1,18 @@
+local function set_paths()
+    local base = os.getenv("LOCALAPPDATA") .. "/nvim"
+    local paths = table.concat(
+        {
+            base .. "/rpre",
+            "$VIMRUNTIME",
+            base .. "/rpost" },
+        ","
+    )
+
+    vim.o.runtimepath = paths
+    vim.o.packpath = paths
+end
+
+local function main()
+    set_paths()
+end
+main()
