86388eb231109d7cec248a1b3526e16231400596
shc <sh@c>
2025-04-26 23:45:26 +0200

    chore(lsp): update gutter-config to new api

--- common/rpre/plugin/external/lsp.lua
+++ common/rpre/plugin/external/lsp.lua
@@ -491,20 +491,22 @@ local function visual()
     end
 
     local function gutter_sign()
-        local d_str = "Diagnostic"
-        local d_types = { "Error", "Warn", "Hint", "Info" }
-        for _, d_type in ipairs(d_types) do
-            local hl_sign = d_str .. "Sign" .. d_type
-            local hl_linenumber = d_str .. d_type
-            vim.fn.sign_define(
-                hl_sign,       -- slight trickery: name the sign as the hl-group itself
-                {
-                    text = "", -- do NOT show any text in gutter
-                    texthl = hl_sign,
-                    numhl = hl_linenumber,
-                }
-            )
-        end
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "",
+                    [vim.diagnostic.severity.WARN] = "",
+                    [vim.diagnostic.severity.INFO] = "",
+                    [vim.diagnostic.severity.HINT] = "",
+                },
+                numhl = {
+                    [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+                    [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+                    [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+                    [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+                },
+            },
+        })
     end
 
     local function fidget()
